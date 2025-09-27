3db028e5eb63dfac3270992a3c6dd1c0f55a2b76
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-09-27 09:55:12 +0530

    create Tracl.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Welcome from './pages/Welcome';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Track from './pages/Track';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Welcome />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/track" element={<Track />} />
       </Routes>
     </div>
   );
