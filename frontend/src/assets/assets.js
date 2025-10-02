f1bccf8f7e791135776ffa8971b166a625895c2a
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-10-02 15:53:29 +0530

    Slider created successfully

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -11,6 +11,13 @@ import i2 from "./i2.png";
 import i3 from "./i3.png";
 import vid1 from "./vid1.mp4";
 import vid2 from "./vid2.mp4";
+import d1 from "./d1.jpg";
+import d2 from "./d2.jpg";
+import d3 from "./d3.jpg";
+import d4 from "./d4.jpg";
+import d5 from "./d5.jpg";
+import d6 from "./d6.jpg";
+
 
 export const assets = {
     img1,
@@ -26,4 +33,10 @@ export const assets = {
     i3,
     vid1,
     vid2,
+    d1,
+    d2,
+    d3,
+    d4,
+    d5,
+    d6,
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Header from '../components/Header'
 import Why from '../components/Why'
 import Video from '../components/Video'
+import Slider from '../components/Slider'
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Why />
-      <Video/>
+     
+      <Slider />
+      
     </div>
   )
 }
