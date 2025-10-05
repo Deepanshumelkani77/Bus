5f38cf2675969ed27f89c417a9d3e1cae54ea3d3
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-10-05 19:17:35 +0530

    try to set AppContetx

---
+++ admin/src/context/AppContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useNavigate, useLocation } from "react-router-dom";
+
+export const AppContext = createContext();
+
+const AppContextProvider = (props) => {
+
+
+
+  const value = {
+  
+  };
+
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
