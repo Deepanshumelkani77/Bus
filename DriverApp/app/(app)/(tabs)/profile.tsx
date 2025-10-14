f59ab01a392720087b724199292ea0f54d526185
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-10-15 00:23:08 +0530

    change all links

--- DriverApp/app/(app)/(tabs)/profile.tsx
+++ DriverApp/app/(app)/(tabs)/profile.tsx
@@ -19,7 +19,7 @@ import { Ionicons } from '@expo/vector-icons';
 
 const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/drx3wkg1h/image/upload';
 const CLOUDINARY_PRESET = 'BusTrac';
-const API_BASE = 'http://10.65.103.156:2000';
+const API_BASE = 'https://bustrac-backend.onrender.com';
 
 interface Driver {
   _id: string;
